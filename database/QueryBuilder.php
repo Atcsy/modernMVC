ad7818a679fb37ae40d071eb4a84f2121fed0175
Katona Attila <katona.skmobil@gmail.com>
2018-11-13 22:41:27 +0100

    fixing typos

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -2,14 +2,14 @@
 
 class QueryBuilder
 {
-    public function __constuct($pdo)
+    public function __construct($pdo)
     {
         $this->pdo = $pdo;
     }
 
     public function selectAll($table)
     {
-    $statement = $this->pdo->prepare('SELECT * FROM {$table}');
+    $statement = $this->pdo->prepare("SELECT * FROM $table");
             $statement->execute();
         
             return $statement->fetchAll(PDO::FETCH_CLASS);
